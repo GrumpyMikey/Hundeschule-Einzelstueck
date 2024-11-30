f95d35aab8aa8221b4ba4facb1a49bb600aaa290
GrumpyMikey <michig@web.de>
2024-11-30 14:37:06 +0100

    Implement lazy loading for videos.

--- js/main.js
+++ js/main.js
@@ -2,16 +2,13 @@
 import { initNavigation } from './modules/navigation.js';
 import { initPreloader } from './modules/preloader.js';
 import { initScrollEffects } from './modules/scroll.js';
+import { initVideo } from './modules/video.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   // Initialisiere alle Module
+  console.log('Initialisiere alle Module');
   initPreloader();
   initNavigation();
   initScrollEffects();
-  console.log('Initialisiere alle Module') // TMP
-});
-
-// TMP
-document.querySelector('.hero__video').addEventListener('error', function(e) {
-  console.error('Video error:', e);
+  initVideo();
 });
---
+++ js/modules/video.js
@@ -0,0 +1,36 @@
+export function initVideo() {
+
+  const video = document.querySelector('.hero__video');
+  const source = video.querySelector('source');
+
+  if (!video || !source) {
+    console.error('Video oder Source-Element nicht gefunden!');
+    return;
+  }
+
+  video.setAttribute('loading', 'lazy');
+
+  const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Video-Quelle setzen und laden
+        source.src = source.dataset.src;
+        video.load();
+
+        video.addEventListener('loadeddata', () => {
+          console.log('Video erfolgreich geladen');
+          video.classList.add('loaded');
+        });
+
+        observer.unobserve(video);
+      }
+    });
+  }, {
+    rootMargin: '50px 0px',
+    threshold: 0.1
+  });
+
+  observer.observe(video);
+
+}
